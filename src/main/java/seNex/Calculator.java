bc7b7ab826f2bcfce40d8e3984236d60845e63de
namyun kim <nykim@hansung.ac.kr>
2016-04-25 16:43:40 +0900

    add 기능 추가

---
+++ src/main/java/seNex/Calculator.java
@@ -0,0 +1,15 @@
+package seNex;
+
+public class Calculator {
+	
+	private int result;
+	
+	public void add(int x, int y) {
+		result = x+y;
+	}
+	
+	public int getResult() {
+		return result;
+	}
+
+}
---
+++ src/test/java/seNex/CalculatorTest.java
@@ -0,0 +1,17 @@
+package seNex;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		Calculator cal = new Calculator();
+		cal.add(10,  20);
+		assertEquals(30, cal.getResult());
+	}
+
+}
+
